0986646d9972d9113c801ca35b336c5d528fb2d1
AliAwan4CHIF <AWA20215@Spengergasse.at>
2023-01-11 12:35:41 +0100

    ChangeResolution geht halbwegs

--- Ballin/Assets/Script/Settings/Resolutionchanger.cs
+++ Ballin/Assets/Script/Settings/Resolutionchanger.cs
@@ -1,11 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Resolutionchanger : MonoBehaviour
 {
-	public void SetResolution(int width, int height, bool fullscreen)
+	public Dropdown resolutionDropdown;
+	public TMP_Text TMP_TextresolutionText;
+
+	private void Start()
 	{
-		Screen.SetResolution(width, height, fullscreen);
+		resolutionDropdown.onValueChanged.AddListener(ChangeResolution);
+	}
+
+	public void ChangeResolution(int resolutionIndex)
+	{
+		switch (resolutionIndex)
+		{
+			case 0:
+				Screen.SetResolution(1280, 720, true);
+				break;
+			case 1:
+				Screen.SetResolution(1920, 1080, true);
+				break;
+		}
+		Debug.Log("Aktuelle Auflösung: " + Screen.width + "x" + Screen.height);
+		TMP_TextresolutionText.text = Screen.width + "x" + Screen.height;
 	}
 }
