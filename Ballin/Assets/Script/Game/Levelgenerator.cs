0445e40020d6f2e9d7ff7e959c8c070530c79fbf
AliAwan4CHIF <AWA20215@Spengergasse.at>
2023-03-03 15:26:05 +0100

    Barrier gemacht, aber es wird nicht auf runs angezeigt, wenn man von der barrier stirbt

--- Ballin/Assets/Script/Game/Levelgenerator.cs
+++ Ballin/Assets/Script/Game/Levelgenerator.cs
@@ -6,6 +6,7 @@ using UnityEngine;
 public class Levelgenerator : MonoBehaviour
 {
     public GameObject Player;
+    public GameObject Tod;
 
     public List<GameObject> Chunks = new List<GameObject>();
     private List<GameObject> ChunkloadedList = new List<GameObject>();
@@ -25,40 +26,51 @@ public class Levelgenerator : MonoBehaviour
         {
             GameObject selected = Chunks[Random.Range(0, Chunks.Count)];
             GameObject loaded = Instantiate(selected);
-            loaded.transform.position = new Vector3(0, i * shifty, i * shiftz);
-            ChunkloadedList.Add(loaded);
-        }
+			GameObject loadedbarrier = Instantiate(Tod);
+			loaded.transform.position = new Vector3(0, i * shifty, i * shiftz);
+			loadedbarrier.transform.position = new Vector3(0, i * shifty - 5, i * shiftz + 5);
+			ChunkloadedList.Add(loaded);
+			ChunkloadedList.Add(loadedbarrier);
+		}
 
-        lastenddisy = startamount * shifty;
+		lastenddisy = startamount * shifty;
         lastenddisz = startamount*shiftz;
     }
 
     // Update is called once per frame
     void Update()
     {
-			if (lastenddisz - Player.transform.position.z < maxdistance)
-            {
-			    shifty = -37.4f - Mathf.Round(GlobalManager.Speed / 2) * 1;
-			    shiftz = 90.15f+  Mathf.Round(GlobalManager.Speed / 2) * 3f;
-                var number = ChunkloadedList.Count;
-                var f = Random.Range(0, Chunks.Count);
-			    if ((ChunkloadedList[number-1].gameObject.name.Contains("WrenchRamp_Segment")))
-			    {
-				    shifty -= Mathf.Round(GlobalManager.Speed / 2f) * 2 ;
-				    shiftz += Mathf.Round(GlobalManager.Speed / 4f) * 10;
+		var number = ChunkloadedList.Count;
+		if (lastenddisz - Player.transform.position.z < maxdistance)
+        {
+		    shifty = -37.4f - Mathf.Round(GlobalManager.Speed / 2) * 1;
+		    shiftz = 90.15f+  Mathf.Round(GlobalManager.Speed / 2) * 3f;
+            var f = Random.Range(0, Chunks.Count);
+			if ((ChunkloadedList[number-1].gameObject.name.Contains("WrenchRamp_Segment")))
+			{
+			    shifty -= Mathf.Round(GlobalManager.Speed / 2f) * 2 ;
+			    shiftz += Mathf.Round(GlobalManager.Speed / 4f) * 10;
 			}
 			GameObject selected = Chunks[f];
-			    
 			selected.transform.position = new Vector3(0, lastenddisy + shifty, lastenddisz + shiftz);
-                GameObject loaded = Instantiate(selected);
-                ChunkloadedList.Add(loaded);
-                GameObject first = ChunkloadedList[0];
-			    Debug.Log(first.transform.position.z);
-			    Destroy(first);
-                ChunkloadedList.Remove(first);
-                lastenddisy += shifty;
-                lastenddisz += shiftz;
-            }
-       
-    }
+            GameObject loaded = Instantiate(selected);
+            ChunkloadedList.Add(loaded);
+
+			GameObject loadedbarrier = Instantiate(Tod);
+			loadedbarrier.transform.position = new Vector3(0, lastenddisy + shifty - 30, lastenddisz + shiftz + 30);
+			ChunkloadedList.Add(loadedbarrier);
+
+			Debug.Log(ChunkloadedList[number - 1].transform.position.z);
+			GameObject first = ChunkloadedList[0];
+			Destroy(first);
+            ChunkloadedList.Remove(first);
+			first = ChunkloadedList[0];
+			Destroy(first);
+			ChunkloadedList.Remove(first);
+			lastenddisy += shifty;
+            lastenddisz += shiftz;
+        }
+		if (Player.transform.position.z > ChunkloadedList[number - 1].transform.position.z - 150)
+			Debug.Log("death");
+	}
 }
